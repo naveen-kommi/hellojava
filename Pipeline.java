4d10e2dcb20a71541117b7ef0232740c74e098e6
naveen-kommi <54269269+naveen-kommi@users.noreply.github.com>
2019-09-11 17:31:17 +0530

    Update Pipeline.java

--- Pipeline.java
+++ Pipeline.java
@@ -1,6 +1,6 @@
 class Pipeline{
 public static void main(String []args){
 System.out.println("hello this is a pipeline project");
-  System.out.println("this is triggered the jobs in a sequence");
+  System.out.println("triggered the jobs in a sequence");
 }
 }
